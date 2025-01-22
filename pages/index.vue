0cd4b0f9e7410aed346fc113165b7387f77a2741
Cassitly <102461124+cassitly@users.noreply.github.com>
2025-01-22 21:17:03 +0700

    Updated text values

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <UButton
+    <!-- <UButton
       disabled
       :loading="loading"
       color="jade"
@@ -11,7 +11,8 @@
         base: 'focus:outline-dashed',
         rounded: 'rounded-full',
       }"
-    />
+    /> -->
+    <h1 class="absolute text-3xl font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" :style="{ color: 'white' }">Initializing NuxtUI</h1>
   </div>
 </template>
 
